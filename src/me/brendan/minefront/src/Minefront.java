f628f08cb308645bc77e87349d103fc494006a85
Brendanr00lz <brendan.defiance@live.com>
2013-08-06 01:23:10 -0400

    Added more stuf for the engine :}

--- src/me/brendan/minefront/src/Minefront.java
+++ src/me/brendan/minefront/src/Minefront.java
@@ -1,29 +1,131 @@
 package me.brendan.minefront.src;
 
+import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import javax.swing.JFrame;
 
 public class Minefront extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private static final String NAME = "Minefront";
 	public static final int WIDTH = 160;
 	public static final int HEIGHT = 120;
 	private static final int SCALE = 5;
-	
+
 	private boolean running = false;
 	private Thread thread;
 	
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+	
 	public void start() {
 		if (running) return;
-		
+
 		running = true;
+		thread = new Thread(this, "Game");
+		thread.start();
 	}
-	
+
 	public void stop() {
+		if (!running) return;
+
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		double delta = 0;
+		double nsPerTick = 1000000000.0 / 60.0;
+		long lastTime = System.nanoTime();
+		long lastTimer = System.currentTimeMillis();
+		int frames = 0;
+		int ticks = 0;
+
+		init();
+
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / nsPerTick;
+			lastTime = now;
+			boolean shouldRender = true;
+
+			while (delta >= 1) {
+				ticks++;
+				tick();
+				delta -= 1;
+				shouldRender = true;
+			}
+			
+			if (shouldRender) {
+				frames++;
+				render();
+			}
+			
+			while (System.currentTimeMillis() - lastTimer > 1000) {
+				lastTimer += 1000;
+				System.out.println(frames + " fps, " + ticks + " ticks");
+				frames = 0;
+				ticks = 0;
+			}
+		}
+	}
+	
+	private void init() {
+		requestFocus();
+	}
+	
+	private void tick() {
 		
 	}
 	
-	public void run() {
+	private void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
 		
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = i;
+		}
+		
+		int ww = WIDTH * SCALE;
+		int hh = HEIGHT * SCALE;
+		int xo = (getWidth() - ww) / 2;
+		int yo = (getHeight() - hh) / 2;
+		Graphics g = bs.getDrawGraphics();
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(image, xo, yo, ww, hh, null);
+		g.dispose();
+		bs.show();
+	}
+	
+	public static void main(String[] args) {
+		Minefront game = new Minefront();
+		game.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		game.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		game.setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+	
+		JFrame frame = new JFrame(Minefront.NAME);
+		frame.setLayout(new BorderLayout());
+		frame.add(game, BorderLayout.CENTER);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setAlwaysOnTop(false);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		
+		game.start();
 	}
 }
